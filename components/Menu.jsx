ae7f785c857ff64ba7c7ef10b7adfd0fcfffee50
Arian Kheradmand <ariankheradmand@gmail.com>
2025-01-09 00:42:40 +0330

    test

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,25 +1,57 @@
 import Image from "next/image";
 import React, { useState, useEffect, useRef } from "react";
 
-let DropDown = ({ endOfPage, status }) => {
+const buttons = ["درباره ما", "گالری", "مراجعه"];
+
+const Gallery = () => {
+  return <div dir="rtl" className="text-white flex flex-col gap-2   border  px-2 py-2 rounded-xl shadow-lg bg-slate-800/20 animate__animated animate__fadeInRight">
+    <label>ثبت نام</label>
+    <div className="w-full border-white border-b-2 border-dashed"></div>
+    <div className="bg-slate-700/80 pr-2 w-24 rounded-lg animate__animated animate__fadeIn">ایمیل</div>
+    <div className="bg-slate-700/80 pr-2 w-24 rounded-lg animate__animated animate__fadeIn">شماره تلفن</div>
+    <div className="bg-slate-700/80 pr-2 w-24 rounded-lg animate__animated animate__fadeIn">گوگل</div>
+  </div>;
+};
+
+let DropDown = ({ endOfPage, status, drawer, setDrawer }) => {
   return (
     <div
-      className={`fixed w-6/12 px-4 py-2 font-thin bg-gray-500/80 
+      dir="rtl"
+      className={`fixed w-full  font-thin 
     ${endOfPage ? "mb-1 bottom-14 right-1" : "mt-1 top-14 right-1"}
     ${
       status
         ? "animate__animated animate__fadeIn"
         : "animate__animated animate__fadeOut"
     }
-    rounded-xl h-fit z-40`}
+     h-fit z-40`}
     >
-      <div className="flex flex-col gap-2">
-
-        <div className="text-white font-bold">Login</div>
-        <div className="text-white font-bold">Signup</div>
-        <div className="text-white">About Us</div>
-        <div className="text-white">Gallery</div>
-        <div className="text-white">Visit</div>
+        <div className=" flex items-start justify-start gap-2 overflow-auto">
+      <div className="flex flex-col gap-2  w-6/12  border px-2 py-2 rounded-xl shadow-lg bg-slate-800/20">
+        <button className="text-white text-right font-bold bg-slate-700/80 shadow-lg pr-2 rounded-lg">
+          ورود
+        </button>
+        <button
+          onClick={(set) => {
+            if (!drawer) {
+              setDrawer(true);
+            } else {
+              setDrawer(false);
+            }
+          }}
+          className="text-white text-right font-bold bg-slate-700/80 shadow-lg pr-2 rounded-lg"
+        >
+          ثبت نام
+        </button>
+        {buttons.map((div) => {
+          return (
+            <button className="text-white text-right hover:text-gray-800  bg-slate-600/70 shadow-lg rounded-lg pr-2 animate__animated animate__fadeInRight">
+              {div}
+            </button>
+          );
+        })}
+      </div>
+      {drawer ? <Gallery /> : null}
       </div>
     </div>
   );
@@ -29,14 +61,15 @@ function Menu({ endOfPage }) {
   const [dropDown, setDropDown] = useState(false);
   const [status, setStatus] = useState(false);
   const dropDownRef = useRef(null);
+  const [drawer, setDrawer] = useState(false);
 
-  // Close dropdown if clicked outside
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (dropDownRef.current && !dropDownRef.current.contains(event.target)) {
         setStatus(false);
         setTimeout(() => {
           setDropDown(false);
+          setDrawer(false);
         }, 500);
       }
     };
@@ -53,7 +86,7 @@ function Menu({ endOfPage }) {
   }, [dropDown]);
 
   return (
-    <div className="flex items-center justify-center relative">
+    <div className="flex items-center justify-center relative ">
       <button
         onClick={() => {
           if (dropDown) {
@@ -73,8 +106,8 @@ function Menu({ endOfPage }) {
               className="animate__animated animate__fadeIn "
               src="/Close.svg"
               alt="Menu"
-              width={25}
-              height={25}
+              width={20}
+              height={20}
             />
           </div>
         ) : (
@@ -82,14 +115,20 @@ function Menu({ endOfPage }) {
             className="animate__animated animate__fadeIn"
             src="/MenuIcon.svg"
             alt="Menu"
-            width={25}
-            height={25}
+            width={20}
+            height={20}
           />
         )}
       </button>
       {dropDown ? (
         <div ref={dropDownRef}>
-          <DropDown endOfPage={endOfPage} status={status} />
+          <DropDown
+            endOfPage={endOfPage}
+            status={status}
+            drawer={drawer}
+            setDrawer={setDrawer}
+          />
+          
         </div>
       ) : null}
     </div>
--- components/Random.jsx
+++ components/Random.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Random = React.forwardRef((props, ref) => {
   return (
     <div ref={ref}className=" flex flex-col mt-36 random-component ">
-      <div className="py-20 px-6 bg-gray-600 text-white">
-        <p>hello my old friend</p>
+      <div className="py-20 px-6 bg-purple-400 text-white">
+        <p>سلام دوست من خوش اومدی</p>
       </div>
       <div className="py-20 px-6 bg-red-300">
-        <p>hello my old friend</p>
+      <p>سلام دوست من خوش اومدی</p>
       </div>
     </div>
   );
